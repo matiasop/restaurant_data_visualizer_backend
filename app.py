80f35086e18918cf8bb6eb9ccdcfd71ffddf4e93
matiasop <matiasop1998@gmail.com>
2022-08-31 22:43:40 -0400

    feat(data): add get all data endpoint

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from src.get_data import fetch_data, compute_date_information
 
 app = Flask(__name__)
+
+data = fetch_data()
+data = compute_date_information(data)
 
 @app.route('/')
 def home():
     return "Hola mundo"
+
+@app.route('/all_data')
+def get_all_data():
+    return data
 
 if __name__ == "__main__":
     app.run()
